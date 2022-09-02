1cd28ec3a9c29ea05900282089c5c0ba0189ba66
TheWhiteMachine <priveiro@gmail.com>
2022-09-02 12:46:16 -0300

    itemlist con promesa PABLO RIVIERO

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+
 import ItemCount from './ItemCount'
+import ItemList from './ItemList'
+
 export const ItemListContainer = ({ greetings }) => {
     return (
         <div>
             {greetings}
 
             <ItemCount stock={5} initial={1} />
+            <ItemList />
         </div>
     )
 }
